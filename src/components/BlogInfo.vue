a86b49b2889cd3738159c2d2ad781e46557aba34
Yuntian <liu2002246@gmail.com>
2022-10-25 21:11:03 -0700

    Update text

--- src/components/BlogInfo.vue
+++ src/components/BlogInfo.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="info_container">
-        <div id="info_content" v-html="content">
+        <div id="info_content" v-html="content2">
         </div>
     </div>
 </template>
@@ -9,7 +9,9 @@
     export default{
         data(){
             return{
-                content:"来自ShaderToy的美丽螺旋<br><br>Saturday Torus <br> Created By mrange <br> <a href='https://www.shadertoy.com/view/fd33zn'>https://www.shadertoy.com/view/fd33zn</a>"
+                content1:"来自ShaderToy的美丽螺旋<br><br>Saturday Torus <br> Created By mrange <br> <a href='https://www.shadertoy.com/view/fd33zn'>https://www.shadertoy.com/view/fd33zn</a>",
+                content2:"来自ShaderToy的辉光管时钟<br><br>Nixie Tube Clock <br> Created By timmaffett & picoplanetdev <br> <a href='https://www.shadertoy.com/view/Dds3WB'>https://www.shadertoy.com/view/Dds3WB</a>"
+            
             }
         }
     }
