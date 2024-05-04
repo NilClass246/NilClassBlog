d6fc76178ee0d0dd108547393e9dc049006675b5
Yuntian <liu2002246@gmail.com>
2024-05-04 00:00:29 -0700

    use github for dreamnoh

--- src/components/AudioCard.jsx
+++ src/components/AudioCard.jsx
@@ -6,11 +6,12 @@ function AudioCard(){
         <div className='fixed audio-card bg-shion rounded'>
             <div className="p-3">正在播放: 夢幻能 〜 Taboo Marionette</div>
             <audio controls
-            src="./dreamnoh.mp3" 
+            src="https://github.com/NilClass246/NilClassBlog/raw/master/dreamnoh.mp3" 
             type="audio/mpeg"
             className='h-5 rounded-none w-full'
             autoPlay 
             loop
+            muted
             ></audio>
         </div>
     )
